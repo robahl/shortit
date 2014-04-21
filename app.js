fac4ca3707982b47aad408ab12ba47ff388ad1d4
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-21 22:48:44 +0200

    Changez here and there

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 var express = require('express'),
-    app = express(),
-    logger = require('morgan'),
-    mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+app = express(),
+logger = require('morgan'),
+mongoose = require('mongoose'),
+Schema = mongoose.Schema;
 
 // Init
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/public')
 mongoose.connect('mongodb://localhost/urlshort')
 
-var urlSchema = new Schema({
-  urlID: String,
-  realUrl: String,
-  clicks: Number
+var UrlShort = mongoose.model('UrlShort', {
+  urlID: String, realUrl: String, clicks: Number
 });
-
-var UrlShort = mongoose.model('UrlShort', urlSchema);
 
 // Middleware and routes
 app.use(logger('dev'));
-
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
-  res.render('index');
+  // var url = new UrlShort({urlID: '123', realUrl: req.params.name});
+  // url.save(function(err) {
+  //   if (err) throw err;
+  //   console.log("Saved: ", url);
+  // });
+
+  res.render('index', {uuid: makeShortStr(3)})
 });
 
 
-app.use(express.static(__dirname + '/public'));
 
 app.listen(3000)
 console.log("Express app started on port 3000");
+
+
+
+// Return alphanumeric string [a-zA-Z0-9]
+function makeShortStr(length) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i=0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
