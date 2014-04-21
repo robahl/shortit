59a559cc684c7f6ae875f2c3e2a66613937a1f4a
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-22 00:34:16 +0200

    Little list of urls

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 app = express(),
 logger = require('morgan'),
+bodyParser = require('body-parser'),
 mongoose = require('mongoose'),
 Schema = mongoose.Schema;
 
@@ -10,23 +11,37 @@ app.set('views', __dirname + '/public')
 mongoose.connect('mongodb://localhost/urlshort')
 
 var UrlShort = mongoose.model('UrlShort', {
-  urlID: String, realUrl: String, clicks: Number
+  urlID: String, realURL: String, clicks: Number
 });
 
 // Middleware and routes
 app.use(logger('dev'));
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 
+// GET '/'
 app.get('/', function(req,res) {
-  // var url = new UrlShort({urlID: '123', realUrl: req.params.name});
-  // url.save(function(err) {
-  //   if (err) throw err;
-  //   console.log("Saved: ", url);
-  // });
-
-  res.render('index', {uuid: makeShortStr(3)})
+  res.render('index')
 });
 
+// POST '/'
+app.post('/', function(req,res) {
+  var url = new UrlShort({
+    urlID: makeShortStr(3),
+    realURL: req.body.url,
+    clicks: 0});
+
+  url.save(function(err) {if (err) throw err})
+
+  res.redirect('/');
+});
+
+// GET '/list'
+app.get('/list', function(req,res) {
+  UrlShort.find(function(err,docs) {
+    res.render('list', {urls: docs});
+  });
+});
 
 
 app.listen(3000)
